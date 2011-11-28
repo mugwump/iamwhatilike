736e6cfb81e26333b9b9f85af9e6514b0b6ef587
Stefan Frank <s.frank@vierundsechzig.de>
2011-11-28 09:49:00 +0100

    Initial import.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Myfavoritethings::Application.config.session_store :cookie_store, key: '_myfavoritethings_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Myfavoritethings::Application.config.session_store :active_record_store
